7adc6fc11016ecdc6cb1e63ac889c4dc5dcd8747
feigebaobei <18515195415@163.com>
2020-07-01 20:59:16 +0800

    完成查验页面基本功能

---
+++ src/components/identityCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="identityCard">
+    <div class="header">
+      <img :src="ic_key" alt="">
+      <span class="name">{{name}}</span>
+    </div>
+    <img src="" alt="" id="bar">
+    <p>{{}}</p>
+    <!-- <h2>基本样式</h2> -->
+    <!-- <basicvue></basicvue> -->
+  </div>
+</template>
+
+<script>
+// import { basicvue } from '@/components/oasiscare'
+import ic_key from '@/assets/ic_key.png'
+import JsBarcode from 'jsbarcode'
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    originText: {
+      tepe: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      ic_key: ic_key
+    }
+  },
+  computed: {
+  },
+  components: {
+    // basicvue
+  },
+  methods: {
+    init () {
+      JsBarcode('#bar', this.originText)
+      // this.getData()
+    },
+    // getData () {}
+  },
+  created () {},
+  mounted () {
+    this.init()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+  .identityCard
+    .header
+      display: flex
+      align-items: center
+      justify-content: center
+
+      img
+        width: 26px
+
+      .name
+        font-weight: 600
+
+    #bar
+      width: 100%
+</style>
--- src/page/claim/checkValidity.vue
+++ src/page/claim/checkValidity.vue
@@ -1,26 +1,109 @@
 <template>
   <div class="checkValidity">
-    <h2>查验页面内容</h2>
+    <!-- <h2>查验页面内容</h2> -->
+    <!-- 请求到数据 -->
+      <!-- 渲染证书 -->
+      <!-- <section class="certifyBox" :style="{background: `url(${idCardBg})`}"> -->
+      <section class="certifyBox" :style="compIdCardBg">
+        <div class="cont">
+          <p class="ap name">{{opCertifyDataItem(certifyData.needHashData.name)}}</p>
+          <p class="ap gender">{{opCertifyDataItem(certifyData.needHashData.gender)}}</p>
+          <p class="ap nation">{{opCertifyDataItem(certifyData.needHashData.nation)}}</p>
+          <p class="ap year">{{opCertifyDataBirth(certifyData.needHashData.birthday)}}</p>
+          <p class="ap month">{{opCertifyDataBirth(certifyData.needHashData.birthday)}}</p>
+          <p class="ap day">{{opCertifyDataBirth(certifyData.needHashData.birthday)}}</p>
+          <p class="ap address">{{opCertifyDataItem(certifyData.needHashData.address)}}</p>
+          <p class="ap identityNumber">{{opCertifyDataItem(certifyData.needHashData.identityNumber)}}</p>
+        </div>
+      </section>
+      <!-- 附加数据 -->
+      <section class="extendData">
+        <p>
+          <span class="key">签发机关：</span>
+          <span class="value">{{opCertifyDataItem(certifyData.needHashData.issueAuthority)}}</span>
+        </p>
+        <p>
+          <span class="key">有效期限：</span>
+          <span class="value">{{opTimeRange(certifyData.needHashData.startTime, certifyData.needHashData.endTime)}}</span>
+        </p>
+      </section>
+      <!-- 链上指纹 -->
+      <section class="chainFingerPrint">
+        <p>链上指纹：</p>
+        <img :src="arrows" alt="" id="bar">
+      </section>
+      <!-- 签名列表 -->
+      <!-- 拥有者 -->
+      <!-- 因为没有udid，所以不渲染。 -->
+      <!-- <identity-card v-if="hashCont" :name="opCertifyDataItem(certifyData.needHashData.name)" :originText="hashCont"></identity-card> -->
+      <!-- 查验说明 -->
+      <section class="ruleDecribe">
+        <p>指纹计算规则说明</p>
+        <img :src="arrows" alt="" @click="changeShowRule" :class="{down: !showRule, up: showRule}">
+        <section class="rule" v-if="showRule">
+          <p>原理: 利用SM3密码散列函数,对于任何一个给定的消息，它都很容易就能运算出散列数值。一个已知的散列数值，难以去推算出原始的消息。对于两个不同的消息，散列数值不同。</p>
+          <p>说明:</p>
+          <p>证书核验采用对每个值求散列值,然后拼接起来对整体求散列值。声明证书的时候确保了证书的多重散列值准确无误的上链了,核验证书的时候只需计算证书本书的多重散列值和链上作对比即可。</p>
+          <p>举例:</p>
+          <p>多重散列值 = SM3(SM3(name)+SM3(age)+SM3(…))</p>
+        </section>
+      </section>
+      <!-- button -->
+      <button @click="checkCertify" class="checkCertify">查验证书</button>
+      <!-- 结果 -->
+      <!-- 计算比对 -->
+    <!-- 请求不到数据 -->
     <!-- <certifyCheck></certifyCheck> -->
     <button @click="openApp">测试唤醒app</button>
   </div>
 </template>
 
 <script>
-// import { certifyCheck } from './certifyCheck.vue'
 import instance from '@/lib/axiosInstance'
+import idCardBg from '@/assets/front_image.jpg'
+import arrows from '@/assets/arrows.png'
+import JsBarcode from 'jsbarcode'
+// import identityCard from '@/components/identityCard.vue'
+import tokenSDKClient from 'token-sdk-client'
 export default {
   props: {},
   data () {
     return {
+      templateId: '0xd74c92c0fe1f03b7b34a1ee2256bb48df13c44accaf0a02330f4b08e46ddb315',
       temporaryId: this.$route.query.temporaryId || '0x700aa5b0bf33da121b885be22d26266a4b90b7eb774bfcf7d5962dbcb3d7e078',
-      certifyData: {}
+      claim_sn: '',
+      certifyData: {
+        needHashData: {
+          address: '',
+          birthday: '',
+          endTime: '',
+          gender: '',
+          identityNumber: '',
+          issueAuthority: '',
+          name: '',
+          nation: '',
+          startTime: ''
+        }
+      },
+      template: {},
+      hashCont: '', // 证书哈希值
+      // idCardBg: idCardBg
+      arrows: arrows,
+      showRule: false, // 是否显示规则
     }
   },
   computed: {
+    compIdCardBg () {
+      return {
+        'background': `url(${idCardBg})`,
+        'background-repeat': 'no-repeat',
+        'background-size': 'contain',
+        'background-position': '50% 50%'
+      }
+    }
   },
   components: {
-    // certifyCheck
+    // identityCard
   },
   methods: {
     init () {
@@ -34,18 +117,273 @@ export default {
           temporaryId: this.temporaryId
         }
       }).then(response => {
+        this.claim_sn = response.data.data.claim_sn
         this.certifyData = response.data.data.certify_data || '{}'
         this.certifyData = JSON.parse(this.certifyData)
+        // 请求证书模板
+        // this.getTemplate()
+        this.renderBarCode(this.claim_sn)
       }).catch(error => {
         alert(error.message)
+        // this.getTemplate()
+        this.renderBarCode(this.claim_sn)
       })
     },
+    // getTemplate (templateId = '0xd74c92c0fe1f03b7b34a1ee2256bb48df13c44accaf0a02330f4b08e46ddb315') {
+    //   // 向链节点云服务请求证书模板
+    //   // tokenSDKClient.getTemplate(templateId)
+    //   // 使用confuseChainServer代理请求证书模板
+    //   instance({
+    //     url: 'proxyChain/getTemplate',
+    //     method: 'post',
+    //     data: {
+    //       templateId: templateId
+    //     }
+    //   })
+    //   .then(response => {
+    //     console.log(response.data)
+    //     let templateStr = response.data.result.meta_cont || '{}'
+    //     this.template = JSON.parse(templateStr)
+    //     // this.renderBarCode("0x700aa5b0bf33da121b885be22d26266a4b90b7eb774bfcf7d5962dbcb3d7e078")
+    //   }).catch(() => {
+    //     let templateStr = '{"title":"身份证","id":"t008","type":"identity","label":"身份证","background":"https://raw.githubusercontent.com/lqxue/picture_list/master/image/20200624102307.png","example":"https://raw.githubusercontent.com/lqxue/picture_list/master/image/20200625161633.png","keys":{"name":{"type":"text","label":"姓名","name":"name","value":""},"gender":{"type":"text","label":"性别","name":"gender","value":""},"nation":{"type":"text","label":"民族","name":"nation","value":""},"birthday":{"type":"text","label":"出生","name":"birthday","value":""},"address":{"type":"text","label":"住址","name":"address","value":""},"identityNumber":{"type":"text","label":"公民身份号码","name":"identityNumber","value":""},"issueAuthority":{"type":"text","label":"签发机关","name":"issueAuthority","value":""},"startTime":{"type":"text","label":"开始时间","name":"startTime","value":""},"endTime":{"type":"text","label":"结束时间","name":"endTime","value":""},"faceFeature":{"type":"img","label":"人脸特征值","name":"faceFeature","value":""},"front":{"type":"img","label":"身份证正面","name":"front","value":""},"back":{"type":"img","label":"身份证反面","name":"back","value":""}},"desc":"姓名$name$性别$gender$民族$nation$出生$birthday$住址$address$公民身份号码$identityNumber$签发机关$issueAuthority$开始时间$startTime$结束时间$endTime$"}'
+    //     this.template = JSON.parse(templateStr)
+    //     // this.renderBarCode("0x700aa5b0bf33da121b885be22d26266a4b90b7eb774bfcf7d5962dbcb3d7e078")
+    //   })
+    // },
     openApp () {
-      console.log('openApp')
       window.location = 'tokentm://open_app'
       setTimeout(function () {
         alert('2s后会进入应用市场')
       }, 2000);
+    },
+    // 根据是否被hash过显示对应数据
+    opCertifyDataItem (val) {
+      if (val.indexOf('0x') !== 0 && val.length !== 38) {
+        return val
+      } else {
+        return '**不公开**'
+      }
+    },
+    // 根据是否被hash过显示对应数据
+    opCertifyDataBirth (val) {
+      if (val.indexOf('0x') === 0 && val.length === 38) {
+        return val
+      } else {
+        return '****'
+      }
+    },
+    // 处理证书的有效期限
+    opTimeRange (startTime, endTime) {
+      let [start, end] = [new Date(Number(startTime)), new Date(Number(endTime))]
+      let [sy, sm, sd, ey, em, ed] = [start.getFullYear(), start.getMonth() + 1, start.getDate(), end.getFullYear(), start.getMonth() + 1, start.getDate()]
+      return `${sy}年${sm}月${sd}日-${ey}年${em}月${ed}日`
+    },
+    // 渲染链上指纹为条形码
+    renderBarCode (claim_sn) {
+      instance({
+        url: 'proxyChain/getClaim',
+        method: 'post',
+        data: {
+          claim_sn: claim_sn
+        }
+      }).then(response => {
+        // console.log(response)
+        this.hashCont = response.data.data.result.hash_cont
+        JsBarcode('#bar', this.hashCont)
+      }).catch(error => {
+        console.log(error)
+      })
+
+    },
+    changeShowRule () {
+      this.showRule = !this.showRule
+    },
+    // 本地证书hash值后比对。同时告诉后端查验次数-1
+    checkCertify () {
+      // 请求证书模板
+      // getTemplate()
+      // let that = this
+      // let p = new Promise((resolve) => {
+      //     console.log('this', this, this.template)
+      //   if (that.template.desc) {
+      //     console.log('this', this)
+      //     resolve(that.template)
+      //   } else {
+      //     // resolve(instance({
+      //     //   url: 'proxyChain/getTemplate',
+      //     //   method: 'post',
+      //     //   data: {
+      //     //     templateId: that.templateId
+      //     //   }
+      //     // })).then(response => {
+      //     //   return response.data.data.
+      //     // })
+
+      //     return instance({
+      //       url: 'proxyChain/getTemplate',
+      //       method: 'post',
+      //       data: {
+      //         templateId: that.templateId
+      //       }
+      //     }).then(response => {
+      //       resolve(response.data.data.result.meta_cont)
+      //     })
+      //   }
+      // })
+
+
+
+
+      // let p = null
+      // if (this.template.desc) {
+      //   p = Promise.resolve(this.template.desc)
+      // } else {
+      //   p = instance({
+      //     url: 'proxyChain/getTemplate',
+      //     method: 'post',
+      //     data: {
+      //       templateId: this.templateId
+      //     }
+      //   }).then(response => {
+      //     this.template = JSON.parse(response.data.data.result.meta_cont)
+      //     return this.template.desc
+      //   })
+      // }
+      // // p.then(response => {
+      // //   // console.log('response', response, response.data.data.result.meta_cont)
+      // //   // this.template = JSON.parse(response.data.data.result.meta_cont)
+      // //   // this.template = JSON.parse(response)
+      // //   // this.template = response
+      // //   // this.template = JSON.parse(this.template)
+      // //   // this.template = response
+      // //   // this.template = response
+      // //   console.log(this.template)
+      // //   return this.template.desc
+      // // })
+      // p.then(desc => {
+      //   // console.log('response', response)
+      //   // return
+      //   // console.log(this.template)
+      //   // console.log(tokenSDKClient)
+
+      //   // let desc = that.template.desc
+      //   let hash = new tokenSDKClient.Keccak(256)
+      //   for (let [key, value] of Object.entries(this.certifyData.needHashData)) {
+      //     let hashStr = ''
+      //     let reg = ''
+      //     if (value.indexOf('0x') === 0 && value.length === 38) { // 已经被hash了
+      //       hashStr = value
+      //     } else {
+      //       reg = new RegExp(`\\$${key}\\$`, 'ig')
+      //       hash.update(value)
+      //       hashStr = hash.digest()
+      //       hash.reset()
+      //     }
+      //     desc.replace(reg, hashStr)
+      //   }
+      //   hash.update(desc)
+      //   let hashContLocal = hash.digest()
+      //   if (hashContLocal === this.hashCont) {
+      //     alert('ko')
+      //   } else {
+      //     alert('no')
+      //   }
+      // }).catch(error => {
+      //   console.log(error)
+      // })
+
+      // if (this.template.desc) {
+
+      // } else {
+      //   let templateId = '0xd74c92c0fe1f03b7b34a1ee2256bb48df13c44accaf0a02330f4b08e46ddb315'
+      //   instance({
+      //     url: 'proxyChain/getTemplate',
+      //     method: 'post',
+      //     data: {
+      //       templateId: templateId
+      //     }
+      //   }).then(response => {
+      //     // console.log(response.data)
+      //     // console.log(response.data.data.result.meta_cont)
+      //     this.template = JSON.parse(response.data.data.result.meta_cont)
+      //     let desc = this.template.desc
+      //     let hash = new tokenSDKClient.Keccak(256)
+      //     for (let [key, value] of Object.entries(this.certifyData)) {
+      //       let reg = new RegExp(`\\$${key}\\$`, 'ig')
+      //       hash.update(value)
+      //       let hashStr = hash.digest()
+      //       desc.replace(reg, hashStr)
+      //       hash.reset()
+      //     }
+      //     hash.update(desc)
+      //     let hashContLocal = hash.digest()
+      //     hash.reset()
+      //     if (hashContLocal === this.hashCont) {
+      //       alert('ok')
+      //     } else {
+      //       alert('no')
+      //     }
+      //     // this.template = JSON.parse(this.template)
+      //   }).catch(error => {
+      //     console.log(error)
+      //   })
+      // }
+
+
+      instance({
+        url: 'proxyChain/getTemplate',
+        method: 'post',
+        data: {
+          templateId: this.templateId
+        }
+      }).then(response => {
+        this.template = JSON.parse(response.data.data.result.meta_cont)
+        let desc = this.template.desc
+        // return desc
+        let hash = new tokenSDKClient.Keccak(256)
+        // console.log('desc', desc)
+        for (let [key, value] of Object.entries(this.certifyData.needHashData)) {
+          let hashStr = ''
+          // let reg = `\$${key}\$`
+          // let reg = new RegExp(`\\$${key}\\$`, 'ig')
+          let reg = '$' + key + '$'
+          // console.log(key, value, reg)
+          desc.replace('$' + key + '$', value)
+          // console.log('desc', desc)
+          if (value.indexOf('0x') === 0 && value.length === 38) { // 已经被hash了
+            hashStr = value
+          } else {
+            // reg = new RegExp(`\\$${key}\\$`, 'ig')
+            // console.log(reg)
+            // console.log(value)
+            hash.update(value)
+            hashStr = hash.digest()
+            // console.log(hashStr)
+            hashStr = '0x' + tokenSDKClient.utils.bytesToStrHex(hashStr)
+            // console.log(hashStr)
+            hash.reset()
+          }
+          // console.log(desc, reg, hashStr)
+          // console.log('desc', desc)
+          desc = desc.replace(reg, hashStr)
+        }
+        console.log(desc)
+        hash.update(desc)
+        let hashContLocal = hash.digest()
+        hashContLocal = '0x' + tokenSDKClient.utils.bytesToStrHex(hashContLocal)
+        hash.reset()
+        console.log(hashContLocal)
+        if (this.hashCont) {
+          alert('ok')
+        }
+      // }).then(desc => {
+      //   console.log(desc)
+      //   let key = 'name'
+      //   let value = '234567'
+      //   desc = desc.replace('$' + key + '$', value)
+      //   console.log(desc)
+      })
     }
   },
   created () {},
@@ -57,6 +395,106 @@ export default {
 
 <style lang="stylus" scoped>
 
-  // .checkValidity
+  .checkValidity
+    width: 700px
+    margin: 0 auto
+
+    .certifyBox
+      display: flex
+
+      .cont
+        position: relative
+        // width: 700px
+        height: 445px
+        // margin: 0 auto
+
+        .ap
+          position: absolute
+
+        .name
+          left: 120px
+          top: 50px
+          width: 120px
+
+        .gender
+          left: 120px
+          top: 106px
+          width: 120px
+
+        .nation
+          left: 280px
+          top: 106px
+          width: 120px
+
+        .year
+          left: 160px
+          top: 160px
+          width: 120px
+
+        .month
+          left: 240px
+          top: 160px
+          width: 120px
+
+        .day
+          left: 306px
+          top: 160px
+          width: 120px
+
+        .address
+          left: 120px
+          top: 220px
+          width: 360px
+
+        .identityNumber
+          left: 210px
+          top: 362px
+          width: 400px
+
+    .extendData
+      margin-bottom: 30px
+
+      p
+        // margin: 10px 0
+
+      .value
+        font-weight: 600
+
+    .chainFingerPrint
+      margin-bottom: 20px
+      img
+        width: 100%
+
+    .ruleDecribe
+      display: flex
+      justify-content: space-between
+      align-items: center
+      flex-wrap: wrap
+
+      .down
+        width: 30px
+        height: 30px
+        transform: rotate(90deg)
+
+      .up
+        width: 30px
+        height: 30px
+        transform: rotate(-90deg)
+
+      .rule
+        width: 100%
+        flex-basis: 100%
+        flex-shrink: 0
+
+    .checkCertify
+      width: 70%
+      background: #57ba9e
+      margin: 0 auto
+      display: block
+      border: none
+      border-radius: 40px
+      padding: 10px
+      color: #fff
+      font-size: 18px
 
 </style>
